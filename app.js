e564347ebe1ecb8cbf64fd6ba01946b2f2ca7eab
kanishksinghpujari <68298682+kanishksinghpujari@users.noreply.github.com>
2021-06-11 21:05:28 +0530

    hogya

--- app.js
+++ app.js
@@ -5,28 +5,45 @@ const bodyParser=require("body-parser");
 const ejs=require("ejs");
 
 const mongoose=require("mongoose");
-const bcrypt=require("bcrypt");
-const saltRounds=10;
+const session=require("express-session");
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
+
+
 
 const app=express();
-mongoose.connect('mongodb://localhost/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(express.static("public"));
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({
   extended:true
+})); 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+mongoose.connect('mongodb://localhost/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex",true);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-
+userSchema.plugin(passportLocalMongoose);
 
 
 
 const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
  //get
  app.get("/",function(req,res){
    res.render("home");
@@ -40,41 +57,50 @@ const User = mongoose.model('User', userSchema);
  });;
 
 app.get("/logout",function(req,res){
+  req.logout();
   res.redirect("/");
-})
+});
+app.get("/secrets",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }
+  else{
+    res.redirect("/login");
+  }
+});
 
 //post
 app.post("/register",function(req,res){
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-    email:req.body.username,
-     password:hash
-    });
-     newUser.save(function(err){
-       if(!err)
-       res.render("secrets");
-     });
-  });
 
+User.register({username:req.body.username},req.body.password,function(err,user){
+  if(err){
+    console.log(err);
+    res.redirect("/register");
+  }
+  else{
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/secrets");
+    })
+  }
+})
 
 });
 
 app.post("/login",function(req,res){
-  User.findOne({email:req.body.username},function(err,foundUser){
-    if(err){
-    console.log("errot");
-    }
-    else{
-    if(foundUser){
-      bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
-    // result == true
-    if(result==true)
-    res.render("secrets");
+const user= new User({
+  username: req.body.username,
+  password:req.body.password
 });
 
-    }
-    }
-  });
+req.login(user,function(err){
+  if(err){
+    console.log(err);
+  }else{
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/secrets");
+    });
+  }
+})
 });
 app.listen(3000,function(){
   console.log("server started on port 3000");
